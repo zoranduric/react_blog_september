b2b27dd6bc5c37a81dc7f380163c8c9199bb14e4
Zoran <zordjuric@gmail.com>
2022-10-05 19:56:37 +0300

    cookie-parser middleware

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import authRouter from './routes/auth.js';
 import postRouter from './routes/posts.js';
 import userRouter from './routes/users.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
