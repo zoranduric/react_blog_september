fc7bbc8c311a2cbae1e1db6fc4e985ecc98316d3
Zoran <zordjuric@gmail.com>
2022-10-09 10:10:21 +0300

    router & controller

--- api/controlers/post.js
+++ api/controlers/post.js
@@ -1,3 +1,24 @@
+import { db } from '../db.js';
+
+export const getPosts = (req, res) => {
+  const q = req.query.cat
+    ? 'SELECT * FROM posts WHERE cat=?'
+    : 'SELECT * FROM posts';
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.send(err);
+    return res.status(200).json(data);
+  });
+};
+export const getPost = (req, res) => {
+  res.json('post controler');
+};
 export const addPost = (req, res) => {
+  res.json('post controler');
+};
+export const deletePost = (req, res) => {
+  res.json('post controler');
+};
+export const updatePost = (req, res) => {
   res.json('post controler');
 };
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import { addPost } from '../controlers/post.js';
+import {
+  getPosts,
+  getPost,
+  addPost,
+  deletePost,
+  updatePost,
+} from '../controlers/post.js';
 
 const router = express.Router();
 
 // pulls from controlers
-router.get('/test', addPost);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+router.post('/', addPost);
+router.delete('/:id', deletePost);
+router.put('/:id', updatePost);
 
 export default router;
