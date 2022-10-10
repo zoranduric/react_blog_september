1e048154e35c7ac699da2f972b6e2f0698e8d9ad
Zoran <zordjuric@gmail.com>
2022-10-10 21:03:18 +0300

    image upload

--- api/index.js
+++ api/index.js
@@ -3,11 +3,28 @@ import authRouter from './routes/auth.js';
 import postRouter from './routes/posts.js';
 import userRouter from './routes/users.js';
 import cookieParser from 'cookie-parser';
+import multer from 'multer';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('image'), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
@@ -16,6 +33,3 @@ app.use('/api/posts', postRouter);
 app.listen(8800, () => {
   console.log('Connected to 8800 port!');
 });
-
-// docker compose up
-//docker compose down
