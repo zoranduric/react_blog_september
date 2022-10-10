1afcd87724ab58a3660546d80c30e96a56dabff9
Zoran <zordjuric@gmail.com>
2022-10-10 19:32:06 +0300

    related posts

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,51 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-const posts = [
-  {
-    id: 1,
-    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-    img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  },
-  {
-    id: 2,
-    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-    img: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  },
-  {
-    id: 3,
-    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-    img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  },
-  {
-    id: 4,
-    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-    img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  },
-];
+const Menu = ({ category }) => {
+  const [posts, setPosts] = useState([]);
+  console.log(`menu ${category}   `);
 
-const Menu = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/?category=${category}`);
+        setPosts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+    console.log('end of use effect');
+  }, [category]);
+
   return (
     <div className='menu'>
       <h1>Other posts</h1>
-      {posts.map((post, id) => (
-        <div className='post' key={post.id}>
-          <div className='img'>
-            <img src={post.img} alt='' />
-          </div>
-          <div className='content'>
-            <Link className='Link' to={`/post/${id}`}>
-              <h1>{post.title}</h1>
+      {posts &&
+        posts.map((post) => (
+          <div className='post' key={post.id}>
+            <img src={post.image} alt='' />
+            <h2>{post.title}</h2>
+            <Link to={`/post/${post.id}`}>
+              <button>Read more</button>
             </Link>
-
-            <button>Read more</button>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
