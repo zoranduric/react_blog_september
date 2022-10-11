18dbeee6d4a4d494fa881adc710c6d8406684464
Zoran <zordjuric@gmail.com>
2022-10-11 21:20:00 +0300

    updating and deleting query

--- api/controlers/post.js
+++ api/controlers/post.js
@@ -22,8 +22,31 @@ export const getPost = (req, res) => {
   });
 };
 export const addPost = (req, res) => {
-  res.json('post controler');
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json('Not authenticated!');
+
+  jwt.verify(token, 'jwtkey', (err, userInfo) => {
+    if (err) return res.status(403).json('Token is not valid!');
+
+    const q =
+      'INSERT INTO posts(`title`, `content`, `image`, `category`, `date`,`uid`) VALUES (?)';
+
+    const values = [
+      req.body.title,
+      req.body.content,
+      req.body.image,
+      req.body.category,
+      req.body.date,
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json('Post has been created.');
+    });
+  });
 };
+
 export const deletePost = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) return res.status(401).json('message: Unauthorized');
@@ -38,5 +61,26 @@ export const deletePost = (req, res) => {
   });
 };
 export const updatePost = (req, res) => {
-  res.json('post controler');
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json('Not authenticated!');
+
+  jwt.verify(token, 'jwtkey', (err, userInfo) => {
+    if (err) return res.status(403).json('Token is not valid!');
+
+    const postId = req.params.id;
+    const q =
+      'UPDATE posts SET `title`=?,`content`=?,`image`=?,`category`=? WHERE `id` = ? AND `uid` = ?';
+
+    const values = [
+      req.body.title,
+      req.body.content,
+      req.body.image,
+      req.body.category,
+    ];
+
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json('Post has been updated.');
+    });
+  });
 };
